52b311f36392cd7d4999a5e43206d1403291d430
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-10 11:22:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Weather from './Weather';
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello from React
+        </h1>
+        <Weather city="Paris"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from 'react-loader-spinner'
+
+
+export default function Weather(props) {
+
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}C`)
+    }
+        let apiKey = "701f06352d61835bc4fc894e7b084629";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+    return (
+        <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    );
+}
